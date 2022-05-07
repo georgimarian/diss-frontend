6af1a7d0c84aa580cb69181dcc2ff3d1f9d1774f
Denis <criste.denis15@yahoo.com>
2022-05-07 10:51:09 +0300

    models updated

--- ui/src/pages/Teachers.tsx
+++ ui/src/pages/Teachers.tsx
@@ -1,77 +1,130 @@
-import Button from '@mui/material/Button';
 import AppPage from '../components/AppPage';
-import CheckIcon from '@mui/icons-material/Check';
-import {RequestStatus, Teacher} from "../components/Models";
+import {RequestStatus, Student, Teacher} from "../components/Models";
+import TeachersTable from "../components/TeachersTable";
+import {Button, Typography} from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
 
-let Popescu = new Teacher("Pop Popescu", "Behavioral therapy", 6, 5)
-let Popescu2 = new Teacher("Pop Popescuu", "Behavioral therapy", 16, 15)
-let Popescu3 = new Teacher("Pop Popescuuu", "Behavioral therapy", 116, 115)
-let Popescu4 = new Teacher("Pop Popescuuuu", "Behavioral therapy", 1116, 1115)
-
-Popescu.id = 1;
-Popescu2.id = 2;
-Popescu3.id = 3;
-Popescu4.id = 4;
-let teachers = [
-    Popescu, Popescu2, Popescu3, Popescu4
-]
-let requests: Map<string, RequestStatus> = new Map<string, RequestStatus>();
-requests.set("1" + Popescu.id, RequestStatus.NO_REQUEST);
-requests.set("1" + Popescu2.id, RequestStatus.NO_REQUEST);
-requests.set("1" + Popescu3.id, RequestStatus.DENIED);
-requests.set("1" + Popescu4.id, RequestStatus.NO_REQUEST);
-console.log(requests.get("11"))
-
-function CanRequest(teachersList: Teacher[]) {
-    return teachersList.map(teacher => requests.get("1" + teacher.id) ?? "" in [RequestStatus.APPROVED, RequestStatus.IN_PROGRESS]).find(x => x)
-}
 
 const Teachers = () => {
-    var canRequest = CanRequest(teachers)
+
+    var s: Student = {
+        id: 1,
+        password: "lorena",
+        name: "denis",
+        email: "criste.denis15@yahoo.com",
+        thesisDescription: "muhaha",
+        requests: [
+            {
+                id: 1,
+                status: RequestStatus.DENIED,
+                description: "muhaha",
+                studentId: 1,
+                teacherId: 3
+            },
+            {
+                id: 1,
+                status: RequestStatus.DENIED,
+                description: "muhaha",
+                studentId: 1,
+                teacherId: 2
+            }
+        ],
+        description: "i am a mothefucker",
+        requestsLeft: 1
+    }
+    var teachers: Teacher[] =
+        [{
+            id: 2,
+            name: "Pop Popescu",
+            interest: "Behavioral therapy",
+            email: "popescu@yahoo.com",
+            enrolledStudents: [],
+            totalPlaces: 15,
+            password: "pass",
+            requests: [
+                {
+                    id: 1,
+                    status: RequestStatus.DENIED,
+                    description: "muhaha",
+                    studentId: 1,
+                    teacherId: 2
+                },
+            ]
+        },
+            {
+                id: 12,
+                name: "Pop Popescu2",
+                interest: "Behavioral therapy",
+                email: "popescu@yahoo.com",
+                enrolledStudents: [],
+                totalPlaces: 15,
+                password: "pass",
+                requests: []
+            },
+            {
+                id: 112,
+                name: "Pop Popescu3",
+                interest: "Behavioral therapy",
+                email: "popescu@yahoo.com",
+                enrolledStudents: [],
+                totalPlaces: 15,
+                password: "pass",
+                requests: []
+            },
+            {
+                id: 1112,
+                name: "Pop Popescu3",
+                interest: "Behavioral therapy",
+                email: "popescu@yahoo.com",
+                enrolledStudents: [],
+                totalPlaces: 15,
+                password: "pass",
+                requests: []
+            },
+            {
+                id: 3,
+                name: "Pop Popescu3",
+                interest: "Behavioral therapy",
+                email: "popescu@yahoo.com",
+                enrolledStudents: [],
+                totalPlaces: 15,
+                password: "pass",
+                requests: [
+                    {
+                        id: 2,
+                        status: RequestStatus.DENIED,
+                        description: "muhaha",
+                        studentId: 1,
+                        teacherId: 3
+                    },
+                ]
+            },
+        ]
+    if (s.requestsLeft > 0)
+        return (
+            <AppPage title='Teachers'>
+
+                <TeachersTable rows={teachers} student={s}/>
+                <Typography variant="h6" sx={{padding: "2px"}} align="left">
+                    Mai ai <Typography sx={
+                    {
+                        color: s.requestsLeft == 1 ? "red" : "green"
+                        , display: "inline"
+                    }}>{s.requestsLeft}</Typography> cereri rămase
+                </Typography>
+            </AppPage>
+        );
     return (
         <AppPage title='Teachers'>
-            <>
-                <p>Teachers</p>
-                <table id="teachers">
-                    <colgroup>
-                        <col span={1} style={{width: "100px"}}/>
-                        <col span={1} style={{width: "100px"}}/>
-                        <col span={1} style={{width: "40px"}}/>
-                        <col span={1} style={{width: "40px"}}/>
-                        <col span={1} style={{width: "80px"}}/>
-                        <col span={1} style={{width: "80px"}}/>
-                    </colgroup>
-                    <thead>
-                    <tr>
-                        <th>Teacher's name</th>
-                        <th>Area of interest</th>
-                        <th>Nr of enrolled students</th>
-                        <th>Nr of available places</th>
-                        <th>Request status</th>
-                        <th>Apply</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {teachers.map(teacher => {
-                        return (
-                            <tr>
-                                <td>{teacher.name}</td>
-                                <td>{teacher.interest}</td>
-                                <td>{teacher.enrolledStudents}</td>
-                                <td>{teacher.availablePlaces}</td>
-                                <td>{requests.get("1" + teacher.id)}</td>
-                                <td>{canRequest && requests.get("1" + teacher.id) == RequestStatus.NO_REQUEST ? <Button variant="outlined" startIcon={<CheckIcon/>}>
-                                    Apply </Button> : ""}
-                                </td>
-                            </tr>
-                        )
-                    })
-                    }
-                    </tbody>
-                </table>
-            </>
+            <Typography variant="h5" sx={{padding: "2px"}} align="left">
+                Mai ai <Typography variant="h5" sx={{color: "red", display: "inline"}}>0</Typography> cereri rămase. Te
+                rugăm să ceri adminilor să îți aleagă un profesor.
+            </Typography>
+            <Button variant="outlined" sx={{color: "red"}} startIcon={<AddIcon sx={{color: "green"}}/>}>
+                Cere ajutorul adminilor </Button>
+
         </AppPage>
-    );
+    )
 }
 
 export default Teachers;
